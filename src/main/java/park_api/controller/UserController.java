d0ee8c3db22f3cab219863d67bbf55e5fc8ed396
Raissa <raissamalves@gmail.com>
2025-11-09 20:32:09 -0300

    feat(controller): add UserService dependency to UserController

--- src/main/java/park_api/controller/UserController.java
+++ src/main/java/park_api/controller/UserController.java
@@ -3,8 +3,13 @@ package park_api.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import park_api.service.UserService;
+
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
-
+    
+    private final UserService userService;
 }
